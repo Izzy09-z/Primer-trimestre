79e580c1945edb1502d76de0eb19f2e20040eda1
Isabella <uribe.garces.isabella@gmail.com>
2025-05-31 17:40:35 -0500

    Nuevo codigo

--- Controladores_de_flujo.py
+++ Controladores_de_flujo.py
@@ -1,21 +1,48 @@
-#EJEMPLO DE IF
+# #EJEMPLO DE IF
  
-edad=int(input("Ingresa una edad: "))
-if edad >=18:
-    print("Si cumple con la condicion, eres mayor de edad")
+# edad=int(input("Ingresa una edad: "))
+# if edad >=18:
+#     print("Si cumple con la condicion, eres mayor de edad")
 
-#EJEMPLO DE ELSE
-numero=24
-if numero>36:
-   print("El numero es grande")
+# #EJEMPLO DE ELSE
+# numero=24
+# if numero>36:
+#    print("El numero es grande")
+# else:
+#     print("El numero es chico")
+
+# #EJEMPLO DE MULTIPLE IF
+# x=25
+# if x > 10:
+#     print("Por encima de diez")
+#     if x > 20:
+#         print("Y tambien por encima de 20!")
+#     else:
+#         print("Pero no por encima de 20")
+
+# #EJEMPLO ELIF
+# num=int(input("Ingresa un numero: "))
+# num1=int(input("Ingresa otro numero: "))
+# oper=num*num1
+# if oper<10:
+#     print("El resultado de los numeros que elegiste es menor que 10")
+# elif oper==20:
+#     print("El resultado de los numeros que elegiste es igual que 20")
+# else:
+#     print("No cumple con la condicion")
+
+#EJEMPLO
+gen=int(input("Ingresa tu año de nacimiento: "))
+
+if gen <= 1945:
+    print("Eres generacion silenciosa")
+elif gen <= 1964:
+    print("Eres generacion Baby Boomer")
+elif gen <= 1979:
+    print("Eres generacion X")
+elif gen <= 2000:
+    print("Eres de la generacion Y")
+elif gen <= 2010:
+    print("Eres de la generacdion Z")
 else:
-    print("El numero es chico")
-
-#EJEMPLO DE MULTIPLE IF
-x=25
-if x > 10:
-    print("Por encima de diez")
-    if x > 20:
-        print("Y tambien por encima de 20!")
-    else:
-        print("Pero no por encima de 20")
+    print("No se encuentra una generacion para tu año")
