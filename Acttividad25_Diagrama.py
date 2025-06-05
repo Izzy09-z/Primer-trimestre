cb62f55dcdd81771b6390c25ff88177bd29fa718
Isabella <uribe.garces.isabella@gmail.com>
2025-06-05 17:43:45 -0500

    Actividad Diagrama de Flujo

---
+++ Acttividad25_Diagrama.py
@@ -0,0 +1,19 @@
+#EJERCIOS DE CONDICIONALES Y OPERACIONES MATEMATICAS
+#Verificacion de numero positivo o negativo
+# num=int(input("Ingrese un numero: "))
+# if num<0:
+#     print("El numero que escribiste es negativo")
+# elif num>0:
+#     print("El numero que escribiste es positivo")
+# else:
+#     print("El numero que escribiste es cero")
+ 
+#Calcular el mayor de dos numeros ingresados
+# numer1=int(input("Ingresa un numero: "))
+# numer2=int(input("Ingresa un segundo numero: "))
+# if numer1<numer2:
+#     print("El primero numero es menor que el segundo")
+# elif numer1>numer2:
+#     print("El primer numero es mayor que el segundo")
+# else:
+#     print("Ambos numeros son iguales")
