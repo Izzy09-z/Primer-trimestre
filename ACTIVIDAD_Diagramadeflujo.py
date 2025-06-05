9aac78f38f7113043693cbade5bf6ab31448b6b7
Isabella <uribe.garces.isabella@gmail.com>
2025-06-05 16:18:50 -0500

    Codigo

--- ACTIVIDAD_Diagramadeflujo.py
+++ ACTIVIDAD_Diagramadeflujo.py
@@ -1,3 +1,5 @@
+# EJERCICIO
+#print("Cuenta Bancaria")
 # nom=(input("Ingresa tu nombre: "))
 # edad=int(input("Ingresa tu edad: "))
 # if edad>=18:
@@ -6,6 +8,7 @@
 #     print("Tu edad no es valida, debes ser mayor de edad para continuar.")
 
 #EJERCICIO 2
+#("Entradas para la feria de acuerdo a la edad")
 # edad= int(input("Ingresa tu edad: "))
 # if edad<4:
 #     print("Entrada gratis")
@@ -17,27 +20,28 @@
 #     print("Tienes que colocar una edad, no una letra")
 
 #EJERCICIO 3
-print("Operaciones disponibles: Suma (+), Resta (-), Multiplicacion (*), Division (/) ")
-num=input(" Ingresa la operacion: ")
-if num == "+":
-    numer= int(input("Ingresa el primer numero para la suma: "))
-    numer2= int(input("Ingresa el segundo numero para completar la suma: "))
-    oper=numer+numer2
-    print("El resultado de la suma fue de: " ,oper)
-elif num=="-":
-    numer3=int(input("Ingresa el primer numero para realizar la resta: "))
-    numer4=int(input("Ingresa el segundo numero para completar la resta: "))
-    oper1=numer3-numer4
-    print("El resultado de la resta es: " ,oper1)
-elif num=="*":
-    numer5= int(input("Ingresa el primer numero para la multiplicacion: "))
-    numer6= int(input("Ingresa el segundo numero para completar la multiplicacion: "))
-    oper2=numer5*numer6
-    print("El resultado de la multiplicacion fue de: " ,oper2)
-elif num=="/":
-    numer7= int(input("Ingresa el primer numero para la Division: "))
-    numer8= int(input("Ingresa el segundo numero para completar la Division: "))
-    oper3=numer7/numer8
-    print("El resultado de la suma fue de: " ,oper3)
-else:
-    print("La operacion es colocaste no está dentro de las opciones anteriores")
+#print("Calculadora")
+#  print("Operaciones disponibles: Suma (+), Resta (-), Multiplicacion (*), Division (/) ")
+# num=input(" Ingresa la operacion: ")
+# if num == "+":
+#     numer= int(input("Ingresa el primer numero para la suma: "))
+#     numer2= int(input("Ingresa el segundo numero para completar la suma: "))
+#     oper=numer+numer2
+#     print("El resultado de la suma fue de: " ,oper)
+# elif num=="-":
+#     numer3=int(input("Ingresa el primer numero para realizar la resta: "))
+#     numer4=int(input("Ingresa el segundo numero para completar la resta: "))
+#     oper1=numer3-numer4
+#     print("El resultado de la resta es: " ,oper1)
+# elif num=="*":
+#     numer5= int(input("Ingresa el primer numero para la multiplicacion: "))
+#     numer6= int(input("Ingresa el segundo numero para completar la multiplicacion: "))
+#     oper2=numer5*numer6
+#     print("El resultado de la multiplicacion fue de: " ,oper2)
+# elif num=="/":
+#     numer7= int(input("Ingresa el primer numero para la Division: "))
+#     numer8= int(input("Ingresa el segundo numero para completar la Division: "))
+#     oper3=numer7/numer8
+#     print("El resultado de la suma fue de: " ,oper3)
+# else:
+#     print("La operacion es colocaste no está dentro de las opciones anteriores")
